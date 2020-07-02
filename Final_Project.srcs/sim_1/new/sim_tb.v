4ef175f5c74adc34299f04b2f508d44653a29d23
DelVel <56226851+DelVel@users.noreply.github.com>
2020-07-02 17:58:51 +0900

    score

--- Final_Project.srcs/sim_1/new/sim_tb.v
+++ Final_Project.srcs/sim_1/new/sim_tb.v
@@ -1,15 +1,17 @@
 `timescale 1ns / 1ps
 
 module sim_tb();
-    reg [3:0]in;
+    reg clk;
     wire [7:0] seg;
-    BCD_7SEG_CONV C(in, seg);
+    wire [3:0]an,out1,out2,out3,out4;
+    Score C(clk,1'b0, seg,an,out1,out2,out3,out4);
 
     integer c;
     initial begin
-       for (c = 0; c<16; c=c+1) begin
-            in = c;
-            #1;
+        clk = 0;
+       for (c = 0; c<32000; c=c+1) begin
+            clk = ~clk;
+            #10;
         end
         $finish;
     end
--- Final_Project.srcs/sources_1/new/Score.v
+++ Final_Project.srcs/sources_1/new/Score.v
@@ -1,47 +1,47 @@
 `timescale 1ns / 1ps
 
-module BCD_Adder(
-    input[3:0] A,
-    input[3:0] B,
-    input Cin,
-    output[3:0] S,
-    output Cout
+module BCD_Counter(
+    input clk, reset,
+    output [3:0]out
     );
-    wire[3:0] _S, _Cin;
-    wire _Cout;
-    Adder_4 add1(A, B, Cin, _S, _Cout);
-    assign Cout = _Cout | (_S[1] & _S[3]) | (_S[2] & _S[3]);
-
-    assign _Cin[0] = 0;
-    assign _Cin[1] = Cout;
-    assign _Cin[2] = Cout;
-    assign _Cin[3] = 0;
-
-    reg temp;
-    Adder_4 add2(_S, _Cin, 0, S, temp);
-endmodule
-
-module Segmentation(
-    input[3:0] IN,
-    output[6:0] OUT
-    );
-    assign OUT[0] = (IN[1] & IN[3]) | (~IN[1] & ~IN[3]) | IN[0] | IN[3];
-    assign OUT[1] = (IN[2] & IN[3]) | (~IN[2] & ~IN[3]) | ~IN[1];
-    assign OUT[2] = IN[1] | ~IN[2] | IN[3];
-    assign OUT[3] = (IN[2] & ~IN[3]) | (~IN[1] & ~IN[3]) | (~IN[1] & IN[2]) |
-    (IN[1] & ~IN[2] & IN[3]) | IN[0];
-    assign OUT[4] = (IN[2] & IN[3]) | (~IN[1] & ~IN[3]);
-    assign OUT[5] = (IN[1] & ~IN[3]) | (~IN[1] & ~IN[3]) | (IN[1] & ~IN[2]) | IN[0];
-    assign OUT[6] = (IN[2] & ~IN[3]) | (IN[2] & ~IN[2]) | IN[0] | (~IN[1] & IN[2]);
-endmodule
+    DFF D1(~reset & ~out[0],~clk,out[0]);
+    DFF D2(~reset & ~out[1] & ~out[3],~out[0],out[1]);
+    DFF D3(~reset & ~out[2], ~out[1], out[2]);
+    DFF D4(~reset & out[2] & out[1], ~out[0], out[3]);
+endmodule // BCD_Counter
 
 module Score(
     input clk, increment, reset,
-    output [0:7]seg,
-    output [3:0]an
+    output [7:0]seg,
+    output reg [3:0]an
     );
-    RAM_4 first(DAT,CLK,WR,CS,OUT);
-    RAM_4 second(DAT,CLK,WR,CS,OUT);
-    RAM_4 third(DAT,CLK,WR,CS,OUT);
-    RAM_4 fourth(DAT,CLK,WR,CS,OUT);
+    wire [3:0] out1,out2,out3,out4;
+    BCD_Counter first(increment | clk & reset,reset,out1);
+    BCD_Counter second(clk & reset | out1[3],reset,out2);
+    BCD_Counter third(clk & reset | out2[3],reset,out3);
+    BCD_Counter fourth(clk & reset | out3[3],reset,out4);
+
+    integer flicker_counter;
+    reg [3:0] targetOut;
+    BCD_7SEG_CONV converter(targetOut, seg);
+
+    initial begin
+        an<=4'b1110;
+        flicker_counter <= 0;
+    end
+    always @(posedge clk) begin
+        if(flicker_counter % 20 == 0) begin
+            case(an)
+                4'b1110 : begin targetOut<=out2; an<=4'b1101; end
+                4'b1101 : begin targetOut<=out3; an<=4'b1011; end
+                4'b1011 : begin targetOut<=out4; an<=4'b0111; end
+                4'b0111 : begin targetOut<=out1; an<=4'b1110; end
+            endcase
+        end
+    end
+    always @(posedge clk) begin
+        if(flicker_counter == 400000000) begin flicker_counter <= 1; end
+        else begin flicker_counter <= flicker_counter + 1; end
+    end
+
 endmodule
